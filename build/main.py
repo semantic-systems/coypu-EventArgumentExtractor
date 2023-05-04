aeff32fbda80d3d7bd6494ccd52e87e32134b260
Junbo Huang <junbo.huang@uni-hamburg.de>
2023-05-04 13:45:54 +0200

    remove ignore

---
+++ build/main.py
@@ -0,0 +1,60 @@
+from flask import Flask, jsonify, request
+from flask_healthz import healthz
+from flair.data import Sentence
+from flair.models import SequenceTagger
+
+
+app = Flask(__name__)
+
+app.register_blueprint(healthz, url_prefix="/healthz")
+
+
+def liveness():
+    pass
+
+
+def readiness():
+    pass
+
+
+app.config.update(
+    HEALTHZ = {
+        "live": app.name + ".liveness",
+        "ready": app.name + ".readiness"
+    }
+)
+
+# load tagger
+tagger = SequenceTagger.load("flair/ner-german")
+
+
+@app.route('/', methods=['POST'])
+def main():
+    authenticated = False
+
+    if 'key' in request.json:
+        key = request.json['key']
+        if (key == '4QT4B4JNCL5SLJM5'): authenticated = True
+
+    if (authenticated == False):
+        response = {'error': 'no valid API key'}
+        http_code = 401
+
+    elif ('message' in request.json):
+        sentence = Sentence(str(request.json['message']))
+        # predict NER tags
+        tagger.predict(sentence)
+        entities = [entity for entity in sentence.get_spans('ner')]
+        response = {"sentence": str(request.json['message']),
+                    'entities': entities}
+        http_code = 200
+
+    else:
+        response = {'error': 'no valid input'}
+        http_code = 400
+
+    return jsonify(response), http_code
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0: 8008')
